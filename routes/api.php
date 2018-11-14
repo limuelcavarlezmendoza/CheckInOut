3998c29c2497290ed8b0938849c2467e05db9ffa
Limuel <limuelcavarlezmendoza@gmail.com>
2018-11-14 11:13:11 +0800

    created Admin & SuperAdmin routes and new controllers

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,46 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    'prefix' => 'api'
+], function () {
+      // route for admin api/superadmin/~
+      Route::group([
+          'prefix' => 'superadmin'
+      ], function () {
+          Route::post('status', 'SuperAdminController@status');//make on/off admin status
+          Route::get('list/admins', 'SuperAdminController@adminList');
+      });
+      // route for admin api/admin/~
+      Route::group([
+          'prefix' => 'admin'
+      ], function () {
+          Route::put('register', 'AdminController@register');//register employee id with his/her Password
+          Route::post('login', 'AdminController@login');
+            // route for approving requests api/admin/aprprove/~
+            Route::group([
+                'prefix' => 'approve'
+            ], function () {
+                Route::put('leave', 'AdminController@approveLeave');
+                Route::put('overtime', 'AdminController@approveOverTime');
+                Route::put('officialbusiness', 'AdminController@approveOfficialBusiness');
+                Route::put('schedule', 'AdminController@approveSchedule');
+            });
+      });
 });
+
+/*
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    //Route::post('login', 'AuthController@login');
+    //Route::post('signup', 'AuthController@signup');
+
+    Route::group([
+      'middleware' => 'auth:api'
+    ], function() {
+    //Route::get('logout', 'AuthController@logout');
+    //Route::get('user', 'AuthController@user');
+    });
+});
+*/
